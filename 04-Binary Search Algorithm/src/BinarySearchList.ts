f89e1a3a722d64185e33ce8e0d555d7f30420c32
mbrezov <127137480+mbrezov@users.noreply.github.com>
2024-01-26 22:45:32 +0100

    Create BinarySearchList.ts

---
+++ 04-Binary Search Algorithm/src/BinarySearchList.ts
@@ -0,0 +1,20 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
+
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2); //middle point of array
+        const v = haystack[m]; // setting pointer to the middle of the array
+
+        if (v === needle) {
+            return true;
+        } else if (v > needle) {
+            // right-hand side is greater than the needle, so reducing high side to that point and excluding the middle
+            hi = m;
+        } else {
+            lo = m + 1; // +1 as we don't need to consider the midpoint as we know that it's not the right value
+        }
+    } while (lo < hi); // do it till pointers get to the same point (collapse)
+
+    return false;
+}
