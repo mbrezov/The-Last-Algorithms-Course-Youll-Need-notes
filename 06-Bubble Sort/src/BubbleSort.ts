71b456ac14620d0846cb8e378bb1b8ceaef29864
mbrezov <127137480+mbrezov@users.noreply.github.com>
2024-02-08 12:19:06 +0100

    Bubble Sort - Update BubbleSort.ts comments

--- 06-Bubble Sort/src/BubbleSort.ts
+++ 06-Bubble Sort/src/BubbleSort.ts
@@ -1,10 +1,10 @@
 export default function bubble_sort(arr: number[]): void {
     for (let i = 0; i < arr.length; ++i) {
         for (let j = 0; i < arr.length - 1 - i; ++j) {
-            // - 1 because we gotta up too but not include also the last element at the first iteration cuz we're gonna do a plus 1
-            // - i every single time we execute the last element becomes the largestelement we don't need to redo that
+            // - 1 because we gotta up too but not include also the last element at the first iteration because we're gonna do a plus 1
+            // - i every single time we execute the last element becomes the largest element we don't need to redo that
             if (arr[j] > arr[j + 1]) {
-                //swap
+                // swap
                 const temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
