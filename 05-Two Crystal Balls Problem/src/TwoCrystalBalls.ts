ab521803d5665c57fac50963c2196146bcac0682
mbrezov <127137480+mbrezov@users.noreply.github.com>
2024-02-08 12:18:05 +0100

    Two Crystal Balls Problem - Update TwoCrystalBalls.ts comments

--- 05-Two Crystal Balls Problem/src/TwoCrystalBalls.ts
+++ 05-Two Crystal Balls Problem/src/TwoCrystalBalls.ts
@@ -1,20 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
     const jumpAmmout = Math.floor(Math.sqrt(breaks.length));
 
-    let i = jumpAmmout; //start at first jump
+    let i = jumpAmmout; // start at first jump
     for (; i < breaks.length; i += jumpAmmout) {
         if (breaks[i]) {
             break;
         }
     }
 
-    //find the point where it breaks, we jump back sqrt(n) then we walk forward sqrt(n)
+    // find the point where it breaks, we jump back sqrt(n) then we walk forward sqrt(n)
     i -= jumpAmmout; // jumped back sqrt(n)
 
-    //walk forward sqrt(n)
+    // walk forward sqrt(n)
     for (let j = 0; j <= jumpAmmout && i < breaks.length; j++, ++i) {
         if (breaks[i]) {
-            return i; //index where it breaks
+            return i; // index where it breaks
         }
     }
     return -1;
