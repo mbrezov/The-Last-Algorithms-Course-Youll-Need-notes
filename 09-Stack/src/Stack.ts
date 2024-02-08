a852d15bb1a80e8ae434c89990bb4e86fe5fa767
mbrezov <127137480+mbrezov@users.noreply.github.com>
2024-02-08 12:21:04 +0100

    Stack - Update Stack.ts comments

--- 09-Stack/src/Stack.ts
+++ 09-Stack/src/Stack.ts
@@ -14,26 +14,26 @@ export default class Stack<T> {
 
     push(item: T): void {
         const node = { value: item } as Node<T>;
-        this.length++; //bookkeeping
+        this.length++; // bookkeeping
         if (!this.head) {
-            this.head = node; //set our head to the node
+            this.head = node; // set our head to the node
             return;
         }
 
-        node.prev = this.head; //take our node and point to the head
-        this.head = node; //take head and point to the new node
+        node.prev = this.head; // take our node and point to the head
+        this.head = node; // take head and point to the new node
     }
 
     pop(): T | undefined {
-        this.length = Math.max(0, this.length - 1); //bookkeeping, we must always stay at 0, can't have negative value as length
+        this.length = Math.max(0, this.length - 1); // bookkeeping, we must always stay at 0, can't have a negative value as a length
         if (this.length === 0) {
-            const head = this.head; //save head
-            this.head = undefined; //we don't need to have head anymore
+            const head = this.head; // save head
+            this.head = undefined; // we don't need to have a head anymore
             return head?.value;
         }
         // we need to detach head
         const head = this.head as Node<T>; // save our point
-        this.head = head.prev; //update head to repoint back to the previous one
+        this.head = head.prev; // update head to repoint back to the previous one
         return head.value;
     }
 
